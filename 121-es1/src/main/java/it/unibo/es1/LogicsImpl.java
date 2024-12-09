133be987be9abf6d778c2b60570865f0f9ce5f35
Gioele Foschi <gioele.foschi@studio.unibo.it>
2024-12-09 10:09:44 +0100

    model done

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,41 +1,49 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private int size;
+	private List<Integer> valuesList;
+	private List<Boolean> enabledList;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.size = size;
+		this.valuesList = new ArrayList<>(this.size);
+		this.enabledList = new ArrayList<>(this.size);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return Collections.unmodifiableList(valuesList);	
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return Collections.unmodifiableList(enabledList);
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int value = this.valuesList.get(elem) + 1;
+		if(value == this.size){
+			this.enabledList.set(elem, false);
+		}
+		return value;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.valuesList.stream().map(i -> Integer.toString(i)).collect(Collectors.joining("|","<<", ">>"));
 	}
 
 	@Override
